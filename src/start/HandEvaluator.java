97aaafcec1d7af0ead8c30ae14b7c87e3965a6d6
Tidus <tidus1215@gmail.com>
2017-06-16 00:47:44 +0800

    bug fix

--- src/start/HandEvaluator.java
+++ src/start/HandEvaluator.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * @author Tidus
+ *
+ */
 public class HandEvaluator {
 
 	private Card cards[];
@@ -17,7 +21,7 @@ public class HandEvaluator {
 	}
 	
 	public void evaluateHand() {
-		byte tallyValues[] = new byte[13];
+		byte tallyValues[] = new byte[14];	
 		byte tallySuits[] = new byte[4];
 		
 		byte fours = 0;
@@ -25,13 +29,13 @@ public class HandEvaluator {
 		byte pair1 = 0;
 		byte pair2 = 0;
 		
-		for (int i=0; i<5; i++) {
-			tallyValues[cards[i].getRank()]++;
-			tallySuits[cards[i].getSuit()]++;
+		for (int i = 0; i < 5; i++) {
+			tallyValues[cards[i].getRank()]++;	//same rank ,index is 1 to 13 
+			tallySuits[cards[i].getSuit()]++;	//same suit
 		}
 		
 		
-		for (byte i=12; i>=0; i--) {
+		for (byte i = 13; i > 0; i--) {	//1 to 13 represent of Two to Ace, 13 numbers
 			if (tallyValues[i] == 4) {
 				fours = i;
 			} else if (tallyValues[i] == 3) {
@@ -53,9 +57,10 @@ public class HandEvaluator {
 		
 		float tempRank = 0;
 		
-		if (sortedCardValues.get(0) + 4 == sortedCardValues.get(4)) {
+		if (sortedCardValues.get(0) + 4 == sortedCardValues.get(4)
+				|| (sortedCardValues.get(4) == 13 && sortedCardValues.get(0) + 3 == sortedCardValues.get(3))) {		//2345A
 			pattern = "Straight";
-			tempRank = 56 + sortedCardValues.get(4);
+			tempRank = 56 + sortedCardValues.get(4) + (float)sortedCardValues.get(3) / 100;
 			for (byte x=0; x<4; x++) {
 				if (tallySuits[x] == 5) {
 					tempRank += 56;
@@ -65,22 +70,22 @@ public class HandEvaluator {
 		}
 		
 		if (fours > 0) {
-			tempRank = 98 + fours;
+			tempRank = 98 + fours + calcAddValue(sortedCardValues, 100, fours, 0);
 			pattern = "Four of a Kind";
 		} else if (threes > 0) {
 			if (pair1 > 0) {
-				tempRank = 84 + threes;
+				tempRank = 84 + threes + (float) pair1 / 100 ;
 				pattern = "Full House";
 			} else {
-				tempRank = 42 + threes;
+				tempRank = 42 + threes  + calcAddValue(sortedCardValues, 100, threes, 0);
 				pattern = "Three of a Kind";
 			}
 		} else if (pair1 > 0) {
 			if (pair2 > 0) {
-				tempRank = 28 + pair1 + (float) pair2 / 100 + (float) sortedCardValues.get(4) / 10000;
+				tempRank = 28 + pair1 + (float) pair2 / 100 + calcAddValue(sortedCardValues, 10000, pair1, pair2);
 				pattern = "Two Pair";
 			} else {
-				tempRank = 14 + pair1 + (float) sortedCardValues.get(4) / 100 + (float) sortedCardValues.get(3) / 10000 + (float) sortedCardValues.get(2) / 1000000;
+				tempRank = 14 + pair1 + calcAddValue(sortedCardValues, 100,  pair1, 0);
 				pattern = "Pair";
 			}
 		}
@@ -100,6 +105,21 @@ public class HandEvaluator {
 		}
 		
 		handValue = tempRank;
+	}
+	
+	
+	/**
+	 * sortedCardValues  1 to 13
+	 */
+	public float calcAddValue(List<Byte> sortedCardValues,int rate, int exclude1, int exclude2){
+		float tempRank = 0;
+		for (int i = 4; i >= 0; i--) {
+			if (sortedCardValues.get(i) == exclude1 || sortedCardValues.get(i) == exclude2)
+				continue;
+			tempRank += (float)  sortedCardValues.get(i) /rate;
+			rate *= 100;
+		}
+		return tempRank;
 	}
 	
 	public String getPokerHandAsString() {
